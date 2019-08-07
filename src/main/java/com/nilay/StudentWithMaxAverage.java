b8e0a79152b0ad1094ae4bcedbcd63fd9548a9a0
nilay <nilayjain14@gmail.com>
2019-08-07 19:08:31 +0530

    added more solution for StudentWithMaxAverage

--- src/main/java/com/nilay/StudentWithMaxAverage.java
+++ src/main/java/com/nilay/StudentWithMaxAverage.java
@@ -7,9 +7,12 @@ public class StudentWithMaxAverage {
 
     public static void main(String[] args) {
 
-        String[][] score = {{"jerry","65"},{"bob","91"}, {"jerry","23"}, {"Eric","83"}};
+        String[][] score = {{"bob","91"}, {"jerry","23"}, {"Eric","83"}, {"jerry","65"}};
         HashMap<String,List<String>> studentAndNumber = new HashMap<>();
+        //if you just wanted to get name and average both then use HashMap approach
         HashMap<String,String> studentAndAve = new HashMap<>();
+        //if you just wanted to get average not the name of the person then use ArrayList approach
+        List<Double> averageList = new ArrayList<>();
 
         for(int r=0;r<score.length;r++){
             if(studentAndNumber.containsKey(score[r][0])){
@@ -23,6 +26,7 @@ public class StudentWithMaxAverage {
             }
         }
 
+
         for(Map.Entry<String,List<String>> map : studentAndNumber.entrySet()){
             List<String> marks = map.getValue();
             Double average = 0.0;
@@ -31,19 +35,24 @@ public class StudentWithMaxAverage {
                 sum += Double.valueOf(mark);
             }
             average = sum/marks.size();
+            //if you just wanted to get average not the name of the person then use a ArrayList approach
+            averageList.add(average);
+            //if you wanted to get name and average both then use HashMap approach
             studentAndAve.put(map.getKey(),String.valueOf(average));
         }
 
+        //if you just wanted to get average not the name of the person then use a ArrayList approach
+        Collections.sort(averageList);
+        System.out.println(averageList.get(averageList.size()-1));
+
+        //if you wanted to get name and average both then use HashMap approach
         final HashMap<String, String> collect = studentAndAve.entrySet()
                 .stream()
                 .sorted(Map.Entry.comparingByValue())
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
                         (e1, e2) -> e1, HashMap::new));
 
-        for(Map.Entry<String,String> map : collect.entrySet()){
-            System.out.println("map.getKey() = "+map.getKey());
-            System.out.println("map.getValue() = "+map.getValue());
-        }
+        System.out.println( collect.entrySet().stream().findFirst().get().getValue());
 
     }
 }
